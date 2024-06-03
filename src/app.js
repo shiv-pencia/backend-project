fe70eaaa2a1b6e8d51eae1d1d1857e0ec64fa841
shiv-pencia <shivprakashpencia@gmail.com>
2024-06-03 21:36:25 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(
@@ -18,5 +19,13 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
 
 export { app };
